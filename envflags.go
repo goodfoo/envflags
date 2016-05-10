3ae7e8b227be42c8a0bb16533fb43c1b2e49ee88
Brad Brown <brad@vendhq.com>
2016-05-10 19:31:34 +1200

    optional upper case transform for ENV vars

--- envflags.go
+++ envflags.go
@@ -9,20 +9,38 @@ package envflags
 import (
 	"flag"
 	"os"
+	"strings"
 )
 
 // FlagSet has-a flag.Flagset
 type FlagSet struct {
 	flag.FlagSet
+	uppercase bool
+}
+
+// New return a new *FlagSet
+func New() *FlagSet {
+	return &FlagSet{}
+}
+
+// Upper builder style uppercase transform
+func (f *FlagSet) Upper() *FlagSet {
+	f.uppercase = true
+	return f
 }
 
 // Parse inject the environment at highest precedence
 func (f *FlagSet) Parse() {
 	// get command line stuff and defaults
 	f.FlagSet.Parse(os.Args[1:])
+
+	transformer := func(s string) string { return s }
+	if f.uppercase {
+		transformer = strings.ToUpper
+	}
 	// inject environment
 	f.VisitAll(func(flag *flag.Flag) {
-		if value, OK := os.LookupEnv(flag.Name); OK {
+		if value, OK := os.LookupEnv(transformer(flag.Name)); OK {
 			flag.Value.Set(value)
 		}
 	})
